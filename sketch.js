170db7101d2557c1bbcfae92d1d46ae908bf5298
mltplxr <128910989+mltplxr@users.noreply.github.com>
2023-11-26 16:03:59 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,21 +2,19 @@ let counter = 0;
 
 function setup() {
   createCanvas(60, 11);
+
   textAlign(CENTER, CENTER);
   textSize(10); 
   textFont('monospace');
 }
 
 function draw() {
-  background(250, 0); 
+  
+  background(200, 30); 
 
   counter += 0.01;
   
-  push(); // Save drawing state
-  resetMatrix(); // Reset transform
-  
-  rect(0, 0, width, height); // Draw white rect over everything
-  fill(0);
+
   text(counter.toFixed(2) + ' kWh', width/2, height/2);
-  pop(); // Restore original state
+
 }
