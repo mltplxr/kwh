a3b38b9b8c3e64fbc764aae69c993d4972fb204d
mltplxr <128910989+mltplxr@users.noreply.github.com>
2023-11-26 14:17:35 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,12 @@
 let counter = 0;
-
 function setup() {
-  createCanvas(400, 200);
-  background(255, 0); // Transparent background
-  textAlign(CENTER, CENTER);
-  textSize(10);
-  textFont('monospace');
+createCanvas(400, 200);
+textAlign(CENTER, CENTER);
+textSize(10);
+textFont('monospace');
 }
-
 function draw() {
-  background(255, 50);
-
-  counter += 0.01; // You can adjust the increment value
-
-  fill(0);
-  text(counter.toFixed(2) + ' kWh', width / 2, height / 2);
+counter += 0.01;
+fill(0);
+text(counter.toFixed(2) + ' kWh', width / 2, height / 2);
 }
